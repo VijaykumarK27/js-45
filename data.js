5e31e9caffb71f547e7c1448c0cf6a0e8f3d043a
VijaykumarK27 <vijaykumar.kadappagol2001@gmail.com>
2023-09-07 21:12:12 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,13 @@
+class Parent{
+    get_Id=101;
+    get_Name='vijay';
+   
+}
+class Child extends Parent{
+   get_bal=500;
+ }
+
+ let a1=new Parent()
+ let a2=new Child()
+ console.log(a1)
+ console.log(a2)
